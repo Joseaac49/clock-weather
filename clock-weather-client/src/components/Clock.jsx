c9529a0e436294b16c5adc1888aa0a5880c25c16
Joseaac49 <tobi1234.ja@gmail.com>
2025-09-30 18:44:17 -0300

    ui: reloj + tarjeta + estilos (front bonito)

--- clock-weather-client/src/components/Clock.jsx
+++ clock-weather-client/src/components/Clock.jsx
@@ -2,24 +2,8 @@ import { useEffect, useState } from 'react';
 
 export default function Clock() {
   const [now, setNow] = useState(new Date());
-
-  useEffect(() => {
-    const id = setInterval(() => setNow(new Date()), 1000);
-    return () => clearInterval(id);
-  }, []);
-
-  const fecha = new Intl.DateTimeFormat('es-AR', {
-    dateStyle: 'full'
-  }).format(now);
-
-  const hora = new Intl.DateTimeFormat('es-AR', {
-    timeStyle: 'medium'
-  }).format(now);
-
-  return (
-    <div className="clock">
-      <div className="clock-time">{hora}</div>
-      <div className="clock-date">{fecha}</div>
-    </div>
-  );
+  useEffect(() => { const id=setInterval(()=>setNow(new Date()),1000); return ()=>clearInterval(id); }, []);
+  const fecha = new Intl.DateTimeFormat('es-AR',{dateStyle:'full'}).format(now);
+  const hora  = new Intl.DateTimeFormat('es-AR',{timeStyle:'medium'}).format(now);
+  return (<div className="clock"><div className="clock-time">{hora}</div><div className="clock-date">{fecha}</div></div>);
 }
