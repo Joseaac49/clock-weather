b6730e2b2df9e45c5ed4abbd1cba804b0169560b
Joseaac49 <tobi1234.ja@gmail.com>
2025-09-30 18:14:01 -0300

    ci(pages): deploy front with GitHub Actions

--- clock-weather-client/vite.config.js
+++ clock-weather-client/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// si tu repo del front es "clock-weather-client"
 export default defineConfig({
   plugins: [react()],
-  base: '/clock-weather-client/'
+  base: '/clock-weather/'   // nombre del repo
 })
